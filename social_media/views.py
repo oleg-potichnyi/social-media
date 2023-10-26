9908b7f0e8f0f5095b6bfa7947834762c1545fe7
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-10-26 11:07:40 +0300

    Added user registration and profile creation views in views.py, implemented user and profile serializers in serializer.py, and defined API endpoints in urls.py.

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from social_media.models import UserProfile
+from social_media.permissions import IsAdminOrIfAuthenticatedReadOnly
+from social_media.serializers import UserSerializer, UserProfileSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserProfileViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class LogoutView(APIView):
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": "Invalid token or token not provided."}, status=status.HTTP_400_BAD_REQUEST)
